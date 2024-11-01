9d7ad84490e390f203a211ac817211b9175fdef5
Jordy McNab <jordy@usecapsule.com>
2024-10-31 19:13:16 -0500

    Updates

--- assets/cpsl-row.entry-Cx16BuGl.js
+++
@@ -1,1 +0,0 @@
-import{l as a,m as s,H as t}from"./index-CbXHIvdA.js";const o=":host{display:flex;flex-wrap:wrap}",c=o,r=class{constructor(e){a(this,e)}render(){return s(t,{key:"29ed5413502c575ff74a496ae6ee83ce375635b0"},s("slot",{key:"864af3ce3db31f98f7effb837fabec43f5a0a6ac"}))}};r.style=c;export{r as cpsl_row};
